f5612e2100e335068f3ff80900b9efeff76929ea
Gaurav_Pandey <33441488+GauravPandey1898@users.noreply.github.com>
2020-08-14 03:25:27 +0530

    Coming Soon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+
+
+      <div class="firefly"></div>
+      <div class="firefly"></div>
+      <div class="firefly"></div>
+      <div class="firefly"></div>
+      <div class="firefly"></div>
+      <div class="firefly"></div>
+      <div class="firefly"></div>
+      <div class="firefly"></div>
+      <div class="firefly"></div>
+      <div class="firefly"></div>
+      <div class="firefly"></div>
+      <div class="firefly"></div>
+      <div class="firefly"></div>
+      <div class="firefly"></div>
+      <div class="firefly"></div>
+      <div>
+        <p className="text_style">Coming Soon </p>
+      </div>
+      <footer class="site-footer">
+
+        <div class="container">
+          <div class="row">
+            <div class="col-md-8 col-sm-6 col-xs-12">
+              <p class="copyright-text">Copyright &copy; 2020 All Rights Reserved by
+              <a href="https://www.gauravpandey.me"> Gaurav Pandey</a>.
+            </p>
+            </div>
+
+            <div className="col-md-4 col-sm-6 col-xs-12">
+              <ul className="social-icons">
+                <li><a className="facebook" href="https://www.facebook.com/devil1898"><i className="fa fa-facebook"></i></a></li>
+                <li><a className="dribbble" href="https://www.instagram.com/12_mp_life/"><i className="fa fa-instagram"></i></a></li>
+                <li><a className="twitter" href="https://github.com/GauravPandey1898"><i className="fa fa-github"></i></a></li>
+                <li><a className="linkedin" href="https://www.linkedin.com/in/gaurav-pandey-111b56150"><i className="fa fa-linkedin"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </main>
   );
 }
 
