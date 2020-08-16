e456bad231ff76a41928d8dc6b958f7ec3cd640e
Gaurav_Pandey <33441488+GauravPandey1898@users.noreply.github.com>
2020-08-16 13:20:58 +0530

    css optimization - icon hover color

--- src/App.js
+++ src/App.js
@@ -38,8 +38,8 @@ function App() {
             <div className="col-md-4 col-sm-6 col-xs-12">
               <ul className="social-icons">
                 <li><a className="facebook" href="https://www.facebook.com/devil1898"><i className="fa fa-facebook"></i></a></li>
-                <li><a className="dribbble" href="https://www.instagram.com/12_mp_life/"><i className="fa fa-instagram"></i></a></li>
-                <li><a className="twitter" href="https://github.com/GauravPandey1898"><i className="fa fa-github"></i></a></li>
+                <li><a className="instagram" href="https://www.instagram.com/12_mp_life/"><i className="fa fa-instagram"></i></a></li>
+                <li><a className="github" href="https://github.com/GauravPandey1898"><i className="fa fa-github"></i></a></li>
                 <li><a className="linkedin" href="https://www.linkedin.com/in/gaurav-pandey-111b56150"><i className="fa fa-linkedin"></i></a></li>
               </ul>
             </div>
