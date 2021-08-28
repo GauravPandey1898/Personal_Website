a16f95526c66cab7032a63699287c8bad593593f
Gaurav_Pandey <33441488+GauravPandey1898@users.noreply.github.com>
2021-08-28 14:54:07 +0530

    added form controller

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,12 +1,8 @@
 import React from "react";
 
 import './Contact.scss'
-
-function encode(data) {
-    return Object.keys(data)
-        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-        .join("&");
-}
+var axios = require("axios");
+var qs = require("qs");
 
 export default class Contact extends React.Component {
     constructor(props) {
@@ -20,18 +16,31 @@ export default class Contact extends React.Component {
 
     handleSubmit = e => {
         e.preventDefault();
-        const form = e.target;
-        fetch("/", {
-            method: "POST",
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: encode({
-                "form-name": form.getAttribute("name"),
-                ...this.state
+        var data = qs.stringify({
+            'entry.1846200855': this.state.name,
+              'entry.1706179443': this.state.email,
+              'entry.419569346': this.state.subject,
+              'entry.310860954': this.state.message
+          });
+          var config = {
+            method: "post",
+            url:
+              "https://docs.google.com/forms/u/0/d/e/1FAIpQLScL0wfLnE83LZaBBILXGFWXgKs6-6Uqxuxf_xCVmGleyqkeQw/formResponse",
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+            data: data,
+          };
+        axios(config).then(res => console.log(res)).catch(err => {
+            this.setState({
+                name: "",
+                email: "",
+                subject: "",
+                message: "",
+            },()=>{
+                alert("Will get back to you soon!!")
             })
         })
-        
-        .then(() => alert("Thank You for Messaging me, will get back to you soon."))
-        .catch(error => alert(error));
     };
 
     render() {
@@ -42,37 +51,26 @@ export default class Contact extends React.Component {
                 </div>
                 <form
                     name="contact"
-                    method="post"
-                    data-netlify="true"
-                    data-netlify-honeypot="bot-field"
-                    onSubmit={this.handleSubmit}
-                    action="/"
-                    
+                    onSubmit={this.handleSubmit}     
                 >
                     <h3>Have any doubt or wanna work together? Just drop a message.</h3>
                     {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
                     <input type="hidden" name="form-name" value="contact" />
                     <div className="row">
-                        <p hidden>
-                            <label>
-                                Don’t fill this out:{" "}
-                                <input name="bot-field" onChange={this.handleChange} />
-                            </label>
-                        </p>
                         <div className="col-md-6">
                             <div className="form-group">
-                                <input type="text" name="name" className="form-control" placeholder="Your Name *" onChange={this.handleChange} />
+                                <input type="text" name="name" value={this.state.name} className="form-control" placeholder="Your Name *" onChange={this.handleChange} />
                             </div>
                             <div className="form-group">
-                                <input type="email" name="email" className="form-control" placeholder="Your Email *" onChange={this.handleChange} />
+                                <input type="email" name="email" value={this.state.email} className="form-control" placeholder="Your Email *" onChange={this.handleChange} />
                             </div>
                             <div className="form-group">
-                                <input type="text" name="subject" className="form-control" placeholder="Subject *" onChange={this.handleChange} />
+                                <input type="text" name="subject" value={this.state.subject} className="form-control" placeholder="Subject *" onChange={this.handleChange} />
                             </div>
                         </div>
                         <div className="col-md-6">
                             <div className="form-group">
-                                <textarea name="message" className="form-control" placeholder="Your Message *" onChange={this.handleChange} style={{ width: "100%", height: "150px" }} />
+                                <textarea name="message" className="form-control" value={this.state.message} placeholder="Your Message *" onChange={this.handleChange} style={{ width: "100%", height: "150px" }} />
                             </div>
                         </div>
                         <div className="form-group">
